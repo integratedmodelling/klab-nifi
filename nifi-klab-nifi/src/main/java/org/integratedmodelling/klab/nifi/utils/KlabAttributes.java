a916e79268be2540e3e373439979eb4180e429b7
AM1729 <arnab.moitra@bc3research.org>
2025-09-09 13:38:24 +0200

    putting things tog

--- nifi-klab-nifi/src/main/java/org/integratedmodelling/klab/nifi/utils/KlabAttributes.java
+++ nifi-klab-nifi/src/main/java/org/integratedmodelling/klab/nifi/utils/KlabAttributes.java
@@ -3,4 +3,6 @@ package org.integratedmodelling.klab.nifi.utils;
 public class KlabAttributes {
     final public static String KLAB_SEMANTIC_TYPES = "klab.semanticTypes";
     final public static String KLAB_URN = "klab.urn";
+    final public static String KLAB_CONTEXT_PROJ = "EPSG:4326";
+    final public static long KLAB_UNRESOLVED_OBS_ID = -1;
 }
