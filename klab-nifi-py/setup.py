dad0b1bdad3e016e474ea7a5bb6828bbe8182d00
AM1729 <arnab.moitra@bc3research.org>
2025-09-11 16:03:01 +0200

    fixes

--- klab-nifi-py/setup.py
+++ klab-nifi-py/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="klab-nifi-py",            
+    name="klab_nifi_py",            
     version="0.0.1-beta",                
     author="Arnab Moitra",
     author_email="arnab.moitra@bc3research.org",
